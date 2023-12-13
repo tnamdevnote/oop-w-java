68470c15ad1e646fb0456a2d5904997972479d9e
Taek Been Nam <taekbeen93@gmail.com>
2023-12-13 11:14:48 -0500

    module_2: lesson 3 Back to Basics

---
+++ module_2/ConstantTest.java
@@ -0,0 +1,10 @@
+package module_2;
+
+public class ConstantTest {
+  public static void main(String[] args) {
+    double average = 4.75;
+    int gpa = (int)average;
+
+    System.out.println(gpa);
+  }
+}
