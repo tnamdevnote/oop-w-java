619eefbc1ab28d37caa23314b6021f4d314e718e
Taek Been Nam <taekbeen93@gmail.com>
2023-12-15 17:16:44 -0500

    module 2: lesson 5 II

--- module_2/FToC.java
+++ module_2/FToC.java
@@ -25,19 +25,44 @@ import java.util.Scanner;
 // }
 
 /** Multiple Prompts and Tokens */
+// public class FToC {
+//   public static void main(String[] args) {
+//     // System.in as input. This will allow constructor to read characters from the device.
+//     Scanner input = new Scanner(System.in); //read keyboard input.
+//     // First Prompt
+//     System.out.print("Enter a Fahrenheit value: ");
+//     // assign input value to fahrenheit variable;
+//     int fahrenheit = input.nextInt();
+//     String day = input.nextLine().trim();
+//     // convert fahrenheit to celsius
+//     double celsius = (5D/9) * (fahrenheit - 32);
+//     // print celsius
+//     System.out.println(day + " Fahrenheit: " + fahrenheit);
+//     System.out.println(day + " Celsius: " + celsius);
+//   }
+// }
+/** Formatting String */
+
 public class FToC {
   public static void main(String[] args) {
     // System.in as input. This will allow constructor to read characters from the device.
     Scanner input = new Scanner(System.in); //read keyboard input.
     // First Prompt
     System.out.print("Enter a Fahrenheit value: ");
-    // assign input value to fahrenheit variable;
     int fahrenheit = input.nextInt();
-    String day = input.nextLine().trim();
+
+    System.out.print("Enter a day of the week: ");
+    String day = input.next();
+
+    System.out.print("Enter your preferred Celsius label(Celsius, Centigrade, or C): ");
+    String ctext = input.next();
+
+
     // convert fahrenheit to celsius
     double celsius = (5D/9) * (fahrenheit - 32);
     // print celsius
-    System.out.println(day + " Fahrenheit: " + fahrenheit);
-    System.out.println(day + " Celsius: " + celsius);
+    System.out.printf("%s Fahrenheit: %d\n", day, fahrenheit);
+    // System.out.println(day + " Celsius: " + celsius);
+    System.out.printf("%s %s: %13.1f\n", day, ctext, celsius);
   }
 }
