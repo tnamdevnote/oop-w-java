5e12455fe9e1523d268d11310c3db14e962108cb
Taek Been Nam <taekbeen93@gmail.com>
2023-12-21 10:40:51 -0500

    module_3: lesson 7 while loop

--- module_3/FToC.java
+++ module_3/FToC.java
@@ -3,20 +3,39 @@ package module_3;
 import java.util.Scanner;
 public class FToC {
     public static void main(String[] args) {
+        final int MAX_TEMP = 140;
+        final int MIN_PARK_TEMP = 70;
+        final int MAX_PARK_TEMP = 90;
+
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter a Fahrenheit value: ");
         int fahrenheit = input.nextInt();
+
+        while (fahrenheit >= MAX_TEMP) {
+            System.out.println("Error: The Fahrenheit value must be lower than " + MAX_TEMP);
+            System.out.print("Please enter another Fahrenheit value: ");
+            fahrenheit = input.nextInt();
+        }
+
         System.out.print("Enter a day of the week: ");
         String day = input.next();
+
         double celsius = (5.0/9) * (fahrenheit - 32);
+
         System.out.println(day + " Fahrenheit: " + fahrenheit);
         System.out.printf("%s %.1f \n", day + " Celsius:", celsius);
 
-        if (fahrenheit >= 70) {
-            System.out.println("Yay! Go to park.");
+        if ((fahrenheit >= 86) && (fahrenheit <= 95)) {
+            System.out.println("Go to the swimming pool");
+        } else if ((fahrenheit >= 70) && (fahrenheit < 86)) {
+            System.out.println("Go to the park");
+        } else if ((fahrenheit >= 50) && (fahrenheit < 70)) {
+            System.out.println("Lounge on the patio");
+        } else if ((fahrenheit >= 32) && (fahrenheit < 50)) {
+            System.out.println("Make a snow man");
         }
-        else {
-            System.out.println("Stay home where there’s wi-fi and learn 1331 online");
-        }
+        
+        System.out.println("Bye");
     }
 }
---
+++ module_3/HelloWorldLoop.java
@@ -0,0 +1,11 @@
+package module_3;
+
+public class HelloWorldLoop {
+  public static void main (String[] args) {
+    int counter = 0;
+    while (counter < 10) {
+      System.out.println("Hello World");
+      counter++;
+    }
+  }
+}
