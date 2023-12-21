48f3567e311f79ff09c74c14ccc1370365ef0551
Taek Been Nam <taekbeen93@gmail.com>
2023-12-21 11:05:31 -0500

    moduel_3 : lesson 7 loop

--- module_3/HelloWorldLoop.java
+++ module_3/HelloWorldLoop.java
@@ -1,11 +1,44 @@
 package module_3;
 
+/** While Loop */
+// public class HelloWorldLoop {
+//   public static void main (String[] args) {
+//     int counter = 0;
+//     while (counter < 10) {
+//       System.out.println("Hello World");
+//       counter++;
+//     }
+//   }
+// }
+
+
+/** Do-While Loop */
+// public class HelloWorldLoop {
+//   public static void main (String[] args) {
+//     int counter = 1;
+//     do {
+//       System.out.println("Hello World!" + counter);
+//       counter++;
+//     } while (counter <= 10);
+//   }
+// }
+
+/** For Loop */
+// public class HelloWorldLoop {
+//   public static void main (String[] args) {
+//     // int counter;
+//     for (int counter = 1; counter <= 10; counter++) {
+//       System.out.println("Hello World! " + counter);
+//     }
+//   }
+// }
+
+/** Nested Loop */
+
 public class HelloWorldLoop {
   public static void main (String[] args) {
-    int counter = 0;
-    while (counter < 10) {
-      System.out.println("Hello World");
-      counter++;
+    for (int i = 50; i > 0; i -= 10) {
+      System.out.print(i);
     }
   }
 }
