b6fcd42276a41e0fde66543240212c2803af2284
Taek Been Nam <taekbeen93@gmail.com>
2024-01-24 14:28:30 -0500

    module_5 setter

--- module_5/InsectClient.java
+++ module_5/InsectClient.java
@@ -6,7 +6,10 @@ public class InsectClient {
     Insect bug1 = new Insect(13, 31, 0);
     System.out.println(bug1.getWeight());
     System.out.println(bug1.getX());
-    System.out.println(bug1.getY());
+    bug1.setX(-314);
+    System.out.println(bug1.getX());
+    bug1.setX(133);
+    System.out.println(bug1.getX());
     System.out.println(Insect.getPopulation());
   }
 }
